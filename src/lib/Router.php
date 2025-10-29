f2fe724a2eba914c7e5d278d05618c93cacdf3e4
Groux Dylan <dylangroux2105@gmail.com>
2025-10-29 01:10:38 +0100

    feat : Lib router for accept dynamic url

--- src/lib/Router.php
+++ src/lib/Router.php
@@ -11,4 +11,15 @@ class Route
         $this->path = $path;
         $this->method = $method;
     }
+
+    public function matchRoute(string $pattern, string $url): array|false
+    {
+        $regex = preg_replace('#\{(\w+)\}#', '([^/]+)', $pattern);
+        $regex = '#^' . $regex . '$#';
+        if (preg_match($regex, $url, $matches)) {
+            array_shift($matches); // Retire le match complet
+            return $matches; // Les paramètres dynamiques
+        }
+        return false;
+    }
 }
